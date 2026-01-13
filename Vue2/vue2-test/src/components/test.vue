521600ecec9f5f5ef93badf6414b5a950a91c1a6
TeddiZen <2576903455@qq.com>
2026-01-14 00:30:07 +0800

    创建了一个Vue2的项目

---
+++ Vue2/vue2-test/src/components/test.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="app">测试test</div>
+</template>
+
+<script>
+export default {
+    name: "TestA",
+    props: {
+        msg: String,
+    },
+};
+</script>
+
+<style></style>
